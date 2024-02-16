7224cdb42811481fd03f158214c4712da4e46231
junyojeo <jjy8479@gmail.com>
2024-02-16 21:04:41 +0900

    [modify] cpp06 ex00 toInt

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -22,8 +22,12 @@ int ScalarConverter::toInt(const std::string &str)
 {
     // 변환이 말이 안되거나, 오버플로우가 발생하는 경우 ImpossibleException 예외를 던진다.
     // 예시: 0, -42, 42
-    int num = std::stoi(str);
-    if (!isdigit(str[0]) || num < std::numeric_limits<int>::min() || std::numeric_limits<int>::max() < num)
+    std::istringstream iss(str); // -> 표준 입력(std::cin)이나 파일로부터 데이터를 읽는 것처럼 처리. (꼼수 느낌)
+    // iss는 공백, +, -, 도 올바르게 처리한다.
+    int num;
+    iss >> num; // 문자열 str에서 정수로 데이터를 읽어서 num에 저장
+    // int num = std::stoi(str);
+    if (iss.fail() || !iss.eof()) // iss으로 변환 과정에서 int max, min을 넘어가는 경우에도 fail이 발생. eof는 끝까지 읽었을 때 true
         throw ImpossibleException();
     return num;
 }
@@ -33,6 +37,7 @@ float ScalarConverter::toFloat(const std::string &str)
     // 변환이 말이 안되거나, 오버플로우가 발생하는 경우 ImpossibleException 예외를 던진다.
     // 0.0f, -4.2f, 4.2f
     // -inff, +inf, nanf
+    float num = std::stof(str);
 }
 
 double ScalarConverter::toDouble(const std::string &str)
