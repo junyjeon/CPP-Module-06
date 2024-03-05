9079beae2ee37891f1073c079d4c1e3cf13a7047
junyojeo <jjy8479@gmail.com>
2024-03-05 19:07:10 +0900

    [feat] cpp06

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -97,8 +97,7 @@ void ScalarConverter::convert(const std::string &originalStr)
 {
     std::string str = originalStr;
 
-    if (!str.empty() && str.find("inf") == std::string::npos
-	&& str[str.length() - 1] == 'f')
+    if (!str.empty() && str.find("inf") == std::string::npos && str[str.length() - 1] == 'f')
         str = str.substr(0, str.length() - 1);
     
 	try
